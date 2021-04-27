1e340022fd9af5f1083125f20d4adc670b6fb0e2
Abdoul Diallo <abdoul.diallo@soat.fr>
2021-04-27 18:28:42 +0200

    Initial version of NoFront generated by JHipster-6.10.5

---
+++ src/test/java/com/mycompany/myapp/web/rest/AccountResourceIT.java
@@ -0,0 +1,39 @@
+package com.mycompany.myapp.web.rest;
+
+import com.mycompany.myapp.NoFrontApp;
+import com.mycompany.myapp.security.AuthoritiesConstants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static com.mycompany.myapp.web.rest.AccountResourceIT.TEST_USER_LOGIN;
+
+/**
+ * Integration tests for the {@link AccountResource} REST controller.
+ */
+@AutoConfigureWebTestClient
+@WithMockUser(value = TEST_USER_LOGIN)
+@SpringBootTest(classes = NoFrontApp.class)
+public class AccountResourceIT {
+    static final String TEST_USER_LOGIN = "test";
+
+    @Autowired
+    private WebTestClient accountWebTestClient;
+
+    @Test
+    @WithMockUser(username = TEST_USER_LOGIN, authorities = AuthoritiesConstants.ADMIN)
+    public void testGetExistingAccount() {
+        accountWebTestClient.get().uri("/api/account")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBody()
+            .jsonPath("$.login").isEqualTo(TEST_USER_LOGIN)
+            .jsonPath("$.authorities").isEqualTo(AuthoritiesConstants.ADMIN);
+    }
+}
